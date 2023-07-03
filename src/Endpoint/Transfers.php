16a75bec70551fdffd8c2931c1794f128a7c3aef
Carsten Tetzlaff <carsten-tetzlaff@web.de>
2023-07-03 08:21:45 +0200

    Fixed typo

--- src/Endpoint/Transfers.php
+++ src/Endpoint/Transfers.php
@@ -13,65 +13,65 @@ use stdClass;
  */
 class Transfers extends FootballClient {
 
-    /**
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function all() {
-        $url = "transfres";
-        return $this->call($url);
-    }
+	/**
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function all() {
+		$url = "transfers";
+		return $this->call($url);
+	}
 
-    /**
-     * @param int $transferId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getById(int $transferId) {
-        $url = "transfers/{$transferId}";
-        return $this->call($url);
-    }
+	/**
+	 * @param int $transferId
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getById(int $transferId) {
+		$url = "transfers/{$transferId}";
+		return $this->call($url);
+	}
 
-    /**
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function latest() {
-        $url = "transfres/latest";
-        return $this->call($url);
-    }
+	/**
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function latest() {
+		$url = "transfers/latest";
+		return $this->call($url);
+	}
 
-    /**
-     * @param string $dateFrom
-     * @param string $dateTo
-     * @return stdClass
-     * @throws ApiRequestException
-     * @throws InvalidDateFormatException
-     */
-    public function getByDateRange(string $dateFrom, string $dateTo) {
-        $dateFrom = FootballApiHelper::verifyDate($dateFrom);
-        $dateTo = FootballApiHelper::verifyDate($dateTo);
-        $url = "transfers/between/{$dateFrom}/{$dateTo}";
-        return $this->call($url);
-    }
+	/**
+	 * @param string $dateFrom
+	 * @param string $dateTo
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 * @throws InvalidDateFormatException
+	 */
+	public function getByDateRange(string $dateFrom, string $dateTo) {
+		$dateFrom = FootballApiHelper::verifyDate($dateFrom);
+		$dateTo   = FootballApiHelper::verifyDate($dateTo);
+		$url      = "transfers/between/{$dateFrom}/{$dateTo}";
+		return $this->call($url);
+	}
 
-    /**
-     * @param int $teamId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getByTeamId(int $teamId) {
-        $url = "transfers/teams/{$teamId}";
-        return $this->call($url);
-    }
+	/**
+	 * @param int $teamId
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getByTeamId(int $teamId) {
+		$url = "transfers/teams/{$teamId}";
+		return $this->call($url);
+	}
 
-    /**
-     * @param int $playerId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getByPlayerId(int $playerId) {
-        $url = "transfers/players/{$playerId}";
-        return $this->call($url);
-    }
+	/**
+	 * @param int $playerId
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getByPlayerId(int $playerId) {
+		$url = "transfers/players/{$playerId}";
+		return $this->call($url);
+	}
 }
